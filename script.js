3034f5485ab63791591c4c74b0e0d74a87774390
ShivamP-97 <shivampathak19972@gmail.com>
2025-11-22 22:56:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,38 @@
+const callStack = [];
+
+function pushStack(funcName) {
+  callStack.push(funcName);
+  console.log(`PUSH: ${funcName} | Current Stack: [${callStack.join(" -> ")}]`);
+}
+
+function popStack(funcName) {
+  const popped = callStack.pop();
+  console.log(`POP:  ${funcName} | Current Stack: [${callStack.join(" -> ")}]`);
+}
+
 function foo() {
-  console.trace("Inside foo");
+  console.trace("Inside foo"); 
+  pushStack("foo");
+
   bar();
+
+  popStack("foo");
 }
 
 function bar() {
-  console.trace("Inside bar");   
+  console.trace("Inside bar");  
+  pushStack("bar");
+
   baz();
+
+  popStack("bar");
 }
 
 function baz() {
-  console.trace("Inside baz");  
+  console.trace("Inside baz");
+  pushStack("baz");
+
+  popStack("baz");
 }
 
 foo();
